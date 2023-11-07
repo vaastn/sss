4c4ed8bb616223eaa6cb48d065249c9efdf5ca46
vaastn <147470746+vaastn@users.noreply.github.com>
2023-11-07 07:44:45 +0300

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,7 @@
+a='kgdwgn (jfdmdndn) jfnnmdccn (odjn)'
+def shorter(a):
+    s=a.count('(')
+    for i in range(s):
+        a[:a.find('(')]+a[:a.find(')')+1:] 
+        print(a)
+shorter(a)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              ````
